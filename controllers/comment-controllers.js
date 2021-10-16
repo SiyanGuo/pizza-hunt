ad586b44994e32bbd831d40ec900eda2b6ff6294
Siyan Guo <siyang@Hey-There.local>
2021-10-15 23:27:35 -0400

    add comment controllers

--- controllers/comment-controllers.js
+++ controllers/comment-controllers.js
@@ -1,10 +1,44 @@
-const {Comment, Pizza} = require('../models');
+const { Comment, Pizza } = require('../models');
 const commentController = {
-    addComment(){
-
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) => {
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ messge: 'No pizza found with this id!' })
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err))
     },
 
-    removeComment(){
+    removeComment({ params }, res) {
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deleteComment => {
+                if (!deletedComment) {
+                    return res.status(404).json({ message: 'No comment with this id!' })
+                }
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    return res.status(404).json({ message: 'No pizza found with this id!' })
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
 
     }
 };
